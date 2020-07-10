a825c008eee40d928982a7731e2aac647567bb52
Chandan746 <Chandansinha007@outlook.com>
2020-07-10 13:28:44 +0530

    Leep year

--- maxmovie/views.py
+++ maxmovie/views.py
@@ -6,11 +6,17 @@ month = {"Jan": 0, "Feb": 31, "Mar": 59, "Apr": 90,
 "Sep": 243, "Oct": 273, "Nov": 304, "Dec": 334}
 
 ## Convert the date in 365 days for easly comparing  
-def convertInDays(dates): 
+def convertInDays(dates,leapyr):   
     dates = dates.split(" ")
     monthVal = month[dates[1].strip().title()]
     datesVal = monthVal+int(dates[0])
-    return datesVal
+    if (leapyr and monthVal >= 59):
+        datesVal+=1
+    if monthVal == 31 and int(dates[0]) == 29:
+        leapyr = True
+    return datesVal,leapyr
+
+
 ## Function for findig the maximum disjoint intervals 
 ## Greedy approaches has been used to solve the problem
 
@@ -18,13 +24,19 @@ def convertInDays(dates):
 def getMaxMovie(request):
     if request.method == 'POST':
         movie_list = request.data["movie_list"]
+        leapyear = False 
         movieInNum = []
         final_list=[]
         for key,val in movie_list.items():
             movieid =  int(key)  
-            movie_start_date = convertInDays(val["start_date"])
-            movie_end_date = convertInDays(val["end_date"])
-            movieInNum.append([movieid,movie_start_date,movie_end_date])
+            movie_start_date = convertInDays(val["start_date"],leapyear)
+            if movie_start_date[1]:
+                leapyear = True
+            movie_end_date = convertInDays(val["end_date"],leapyear)
+            if movie_end_date[1]:
+                leapyear = True 
+            if (movie_start_date[0] < movie_end_date[0]):
+                movieInNum.append([movieid,movie_start_date[0],movie_end_date[0]])
         movieInNum.sort(key = lambda x: x[2])
         final_list.append(movieInNum[0])
         last_work_day = movieInNum[0][2]
